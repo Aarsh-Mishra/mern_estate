800b937bd04fb7232ace272319ab9c39ce92aed4
Aarsh <aarshm21@gmail.com>
2025-05-27 22:46:49 +0530

    create sign in api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -53,9 +53,24 @@ function SignUp() {
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        <input type='text' placeholder='username' onChange={handleChange} className='border p-3 rounded-lg' id='username'/>
-        <input type='email' placeholder='email' onChange={handleChange} className='border p-3 rounded-lg' id='email'/>
-        <input type='password' placeholder='password' onChange={handleChange} className='border p-3 rounded-lg' id='password'/>
+        <input 
+        type='text' 
+        placeholder='username' 
+        onChange={handleChange} 
+        className='border p-3 rounded-lg' 
+        id='username'/>
+        <input 
+        type='email' 
+        placeholder='email' 
+        onChange={handleChange} 
+        className='border p-3 rounded-lg' 
+        id='email'/>
+        <input 
+        type='password' 
+        placeholder='password' 
+        onChange={handleChange} 
+        className='border p-3 rounded-lg' 
+        id='password'/>
         <button disabled = {loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
           {loading ? 'Loading...' : 'Sign up'}
         </button>
