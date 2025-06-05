8fcdf210c50811f3577261453cfad5737ddb2ce6
Aarsh <aarshm21@gmail.com>
2025-06-05 10:46:09 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from '../utils/error.js';
 
 export const createListing = async (req, res, next) => {
   try {
@@ -10,14 +11,16 @@ export const createListing = async (req, res, next) => {
   }
 };
 
+
 export const deleteListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
-  console.log(req);
-  console.log(listing);
+  // Check if listing exists
+  // Check if the user is the owner of the listing
   if (!listing) {
     return next(errorHandler(404, "Listing not found!"));
   }
-
+  // Check if the user is the owner of the listing
+  // If the user is not the owner, return an error
   if (req.user.id !== listing.userRef) {
     return next(errorHandler(401, "You can only delete your own listings!"));
   }
@@ -26,6 +29,33 @@ export const deleteListing = async (req, res, next) => {
     await Listing.findByIdAndDelete(req.params.id);
 
     res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listings!"));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+
+      req.body,
+
+      { new: true }
+    );
+
+    res.status(200).json(updatedListing);
   } catch (error) {
     next(error);
   }
