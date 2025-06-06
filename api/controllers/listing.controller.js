e85bbe133382daaf120f0a8f0bb8005620d1e633
Aarsh <aarshm21@gmail.com>
2025-06-06 10:13:58 +0530

    create search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -72,3 +72,77 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+
+    const startIndex = parseInt(req.query.startIndex) || 0;
+
+    let offer = req.query.offer;
+
+    // If offer is not defined or is 'false', set it to include both true and false
+    // This allows us to filter listings based on the offer status
+    if (offer === undefined || offer === "false") {
+      offer = { $in: [false, true] };
+    }
+
+    // If furnished is not defined or is 'false', set it to include both true and false
+    // This allows us to filter listings based on the furnished status
+    let furnished = req.query.furnished;
+
+    if (furnished === undefined || furnished === "false") {
+      furnished = { $in: [false, true] };
+    }
+
+    // If parking is not defined or is 'false', set it to include both true and false
+    // This allows us to filter listings based on the parking availability
+
+    let parking = req.query.parking;
+
+    if (parking === undefined || parking === "false") {
+      parking = { $in: [false, true] };
+    }
+
+    // If type is not defined or is 'all', set it to include both 'sale' and 'rent'
+    // This allows us to filter listings based on the type of listing (sale or rent)
+
+    let type = req.query.type;
+
+    if (type === undefined || type === "all") {
+      type = { $in: ["sale", "rent"] };
+    }
+
+    // Extract search term, sort field, and order from query parameters
+    // This allows us to filter listings based on the search term, sort by a specific field, and order the results
+
+    const searchTerm = req.query.searchTerm || "";
+
+    const sort = req.query.sort || "createdAt";
+
+    const order = req.query.order || "desc";
+
+    const listings = await Listing.find({
+      // Filter listings based on the search term, offer status, furnished status, parking availability, and type
+      name: { $regex: searchTerm, $options: "i" },
+
+      offer,
+
+      furnished,
+
+      parking,
+
+      type,
+    })
+
+      .sort({ [sort]: order })
+
+      .limit(limit)
+
+      .skip(startIndex);
+
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
