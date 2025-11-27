cf3e152f6bf0a5e7dd0f35c5a738abdfa4482729
Aarsh <aarshm21@gmail.com>
2025-11-27 18:55:14 +0530

    fix port in index file.

--- api/index.js
+++ api/index.js
@@ -34,8 +34,11 @@ app.get('*', (req, res)=>{
     res.sendFile(path.join(__dirname, '/client', 'dist', 'index.html'));
 });
 
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
 app.use((err, req, res, next)=>{
